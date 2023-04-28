b935d4079a81543ff1be5449f61d2f8f506d8b65
Tom <tom.whitwell@gmail.com>
2023-04-28 07:46:52 +0100

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -37,7 +37,7 @@
   $editMode = false;
   let configDirty = false;
   let upgradeString = "";
-  let tips = ["Set a channel to 0 to disable it","Read the instructions on the back!", "Click Edit Config for device options", "Ignore those eject warnings", "The boot folder is called MT_BOOT"]
+  let tips = ["Set a channel  to 0 to disable it","Read the instructions on the back!", "Click Edit Config for device options", "Ignore those eject warnings", "The boot folder is called MT_BOOT"]
   let tip = tips[Math.floor(Math.random() * tips.length)]
 
 
